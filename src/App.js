9cc06fefc3983846e1e9665b881bb4ef68e33c02
SeNewGit <seemilyrose@gmail.com>
2023-01-08 00:09:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+
+  function reloadPage() {
+    // window.location.reload(false);
+    window.location.href = ""
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        < button onClick = {
+          reloadPage
+        } >
+          Reload Page
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
